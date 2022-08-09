fdc22a1fe96f15a835bb069337d05459d14ae876
lCrazyblindl <lcrazyblindl@gmail.com>
2022-08-09 23:14:06 +0500

    assert updated

--- lesson_3_6_step9/test_items.py
+++ lesson_3_6_step9/test_items.py
@@ -8,8 +8,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_buy_button_presence(browser):
-    browser.get(' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     assert WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'button.btn-add-to-basket'))
-    , 'Buy button was not found')
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'button.btn-add-to-basket')),
+        'Buy button was not found')
 
