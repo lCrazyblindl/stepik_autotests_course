ab2a515090d785166541fd2dfa4f5f0e3ac74b93
lCrazyblindl <lcrazyblindl@gmail.com>
2022-08-09 16:18:09 +0500

    assert updated

--- lesson_3_6_step9/test_items.py
+++ lesson_3_6_step9/test_items.py
@@ -1,13 +1,15 @@
 import time
-
+from selenium.common.exceptions import NoSuchElementException
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_buy_button_presence(browser):
-    browser.implicitly_wait(60)
     browser.get(' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    buy_button = browser.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
-    time.sleep(5)
-    assert buy_button
+    assert WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'button[type="submit"]'))
+    , 'Buy button was not found')
+
